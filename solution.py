577bff2ac0b65eee96fffa2f3b5693aed3324ecd
eltahsow <60940537+eltahsow@users.noreply.github.com>
2021-12-11 17:33:54 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,11 @@
 from socket import *
-import base64
-
 
 def smtp_client(port=1025, mailserver='127.0.0.1'):
     msg = "\r\n My message"
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-
+    mailserver = ("es5754@nyu.edu, 1025)
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket = socket(AF_INET,SOCK_STREAM)
     clientSocket.connect(mailserver)
